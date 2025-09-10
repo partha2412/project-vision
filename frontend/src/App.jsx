369baaadf9f0879c552afdd1c63e008226d55bc0
Partha Singh <parthasingh35@gmail.com>
2025-09-10 14:31:53 +0530

    all

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Products from './pages/Products'
-import SingleProduct from './pages/SingleProduct'
-import Products2 from './pages/Products2'
-import Test from './pages/Test'
 import Landing from './pages/Landing'
 import NavBar from './common/NavBar'
 import Checkout from './pages/Checkout'
 import VTO from './pages/VTO'
-import CalibrationTool from './pages/CalibrationTool'
 import Footer from './common/Footer'
 import ProductsPage from './pages/ProductsPage'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
-import Airflex from './pages/Airflex'
 import WishlistPage from './pages/WishlistPage'
-//import Navbar2 from './common/Navbar2'
 
 const App = () => {
   return (
@@ -34,12 +27,8 @@ const App = () => {
 
 
 
-          <Route path='/pro' element={<SingleProduct />} />
-          <Route path='/test' element={<Test />} />
           <Route path='/checkout' element={<Checkout />} />
           <Route path='/vto' element={<VTO />} />
-          <Route path='/cal' element={<CalibrationTool />} />
-          <Route path='/airflex' element={<Airflex/>} />
           
         </Routes>
       </BrowserRouter>
