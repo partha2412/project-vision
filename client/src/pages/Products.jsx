af0e855ac5c198a9ce0df2cd4e28922f7e2ae39f
Partha Singh <parthasingh35@gmail.com>
2025-09-13 23:00:22 +0530

    new things

--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -28,7 +28,7 @@ const Products = ({ data }) => {
       
       {/* Responsive Grid */}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 w-full max-w-7xl">
-        {/* one products loop */}
+        
         {data.map((item, index) => (
           <div
             key={index}
--- server/routes/auth.js
+++
@@ -1,6 +0,0 @@
-import express from 'express'
-import { login, signup } from '../controllers/authControllers.js';
-const router = express.Router();
-router.get('/login',login)
-router.get('/signup',signup)
-export default router;
