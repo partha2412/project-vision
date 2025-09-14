bc317a6402e80a05e698235895186828573e023e
Partha Singh <parthasingh35@gmail.com>
2025-09-14 10:11:29 +0530

    products hover fixed

--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -27,7 +27,7 @@ const Products = ({ data }) => {
       </h1>
       
       {/* Responsive Grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2  0 w-full max-w-7xl">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-20  0 w-full max-w-7xl">
         
         {data.map((item, index) => (
           <div
