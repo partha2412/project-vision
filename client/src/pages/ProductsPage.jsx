931b399f7809b2685034ef648a475c878f614864
Partha Singh <parthasingh35@gmail.com>
2025-09-14 13:08:47 +0530

    options button color change, option bar pad added

--- client/src/pages/ProductsPage.jsx
+++ client/src/pages/ProductsPage.jsx
@@ -16,8 +16,8 @@ const ProductsPage = () => {
 
     return (
         <div className='flex relative'>
-            <div className="absolute z-50">
-                <button onClick={() => setShowOptions(!showOptions)} className="p-4 bg-gradient-to-r from-violet-400/70 to-blue-400/40">
+            <div className="absolute left-4 top-4 z-50">
+                <button onClick={() => setShowOptions(!showOptions)} className="p-4 bg-gradient-to-r from-purple-500 to-pink-400">
                     <div className="flex flex-col gap-1">
                         <div className="w-7 h-1 p-[2px] bg-pink-100"></div>
                         <div className="w-7 h-1 p-[2px] bg-pink-100"></div>
@@ -25,7 +25,7 @@ const ProductsPage = () => {
                     </div>
                 </button>
             </div>
-            <div className={`absolute z-30 top-0 left-0 h-full w-64 flex bg-white shadow-lg transform transition-transform duration-300 ease-in-out
+            <div className={`absolute z-30 top-4 left-0 px-4 h-full w-80 flex bg-white shadow-lg transform transition-transform duration-300 ease-in-out
           ${showOptions ? "translate-x-0" : "-translate-x-full"}`}>
                 <LeftOptions />
             </div>
