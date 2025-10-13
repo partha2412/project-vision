0434f326227e2a78f2a8e04d268e6425fa109636
Partha Singh <parthasingh35@gmail.com>
2025-10-13 11:28:23 +0530

    delete products

--- Backend/routes/product.js
+++ Backend/routes/product.js
@@ -37,9 +37,12 @@ router.get('/range', productController.getProductsByRange);
 
 
 // product delete (soft delete)
+// example: /api/product/delete/soft/:id
 router.delete('/delete/soft/:id', productController.softDeleteProduct);
 
 // Hard delete
+// example: /api/product/delete/hard/:id
+// example: /api/product/delete/hard/:id
 router.delete('/delete/hard/:id', productController.hardDeleteProduct);
 
 module.exports = router;
