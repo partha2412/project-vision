ff6252dbbcc13759b3f011cec837fa92598686f5
Partha Singh <parthasingh35@gmail.com>
2025-10-15 14:39:30 +0530

    new changes

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { register,login,logout,updateUser,deleteUser } = require('../controllers/authcontrollers');
 const { isAuthenticated } = require('../middleware/auth');
-
+ 
 router.post('/register', register);
 router.post('/login', login);
 router.get('/logout', logout);
