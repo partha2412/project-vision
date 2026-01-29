4c01594c5f88fa39af2a28209fe8d711ae0555cc
Partha Singh <parthasingh35@gmail.com>
2026-01-29 18:21:14 +0530

    site scaling revert

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,7 +23,7 @@ import AdminDashboard from './pages/AdminDashboard';
 
 const App = () => {
   return (
-    <div className='relative w-screen h-screen'>
+    <div className='relative h-screen'>
       <BrowserRouter>
         <> {/* ✅ Wrap everything needing wishlist */}
           <NavBar />
